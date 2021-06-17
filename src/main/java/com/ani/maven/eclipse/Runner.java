99a751ab165c277b8bbdb0289cbf0fb359911cbe
Aniruthan Ramadoss <55056278+AniRamadoss@users.noreply.github.com>
2021-06-17 17:54:57 -0400

    Update Runner.java

--- src/main/java/com/ani/maven/eclipse/Runner.java
+++ src/main/java/com/ani/maven/eclipse/Runner.java
@@ -3,6 +3,7 @@ package com.ani.maven.eclipse;
 public class Runner {
 
     public static void main(String[] args) {
+        //Random Comment for commit to trigger workflow
         Bot scarlet = new Bot();
 
     }
